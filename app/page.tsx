56980b51e88f67485163aac76d8c67822a0125cc
PramisGrg <gurungpramis361@gmail.com>
2024-11-30 23:43:23 +0545

    navabr

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,20 @@
+import { NavigationMenuDemo } from "./components/navbar";
+
 export default function Home() {
   return (
     <div>
-      <h1>My name is Pramis</h1>
+      <NavigationMenuDemo />
+      <h1>
+        My name is Pramis Lorem ipsum dolor sit, amet consectetur adipisicing
+        elit. Saepe explicabo molestiae consectetur mollitia odit, maxime est
+        eaque quam ad consequuntur sunt obcaecati rerum itaque velit animi
+        similique maiores recusandae quidem.
+      </h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam quaerat
+        quas laborum? Numquam rerum cum temporibus. Harum incidunt praesentium
+        facere, dolores excepturi consectetur odio eum vitae, et ipsam non amet.
+      </p>
     </div>
   );
 }
